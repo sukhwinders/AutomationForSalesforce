7cbb1ce6a8c364434cfc07d698b394348b48578f
Sukhwinder Singh <sukhwinders@smartdatainc.net>
2015-12-29 10:28:14 +0530

    New files commited

---
+++ iCiXSalesforceAutomation/src/main/java/utils/TestBase.java
@@ -0,0 +1,41 @@
+package utils;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.annotations.*;
+import pageObjects.SF_Create_Product_page;
+import pageObjects.SF_HomePage;
+
+
+
+public class TestBase {
+
+	
+	  protected static WebDriver driver;
+	   protected String baseUrl;
+	   protected SF_HomePage sf_homepage;
+	   protected SF_Create_Product_page sf_create_product_page;
+
+
+   @BeforeMethod
+@BeforeSuite
+   public void setUp() {
+         baseUrl = "https://login.salesforce.com/";
+         driver = new FirefoxDriver();
+         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+
+   }
+ 
+   @AfterMethod
+@AfterSuite
+   public void tearDown() throws Exception {
+         driver.quit();
+
+   }
+}
+
