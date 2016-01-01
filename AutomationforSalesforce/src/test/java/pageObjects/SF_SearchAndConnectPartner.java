bcaab2a6315fe6217da6963abe322f1995d5491d
Sukhwinder <sukhwinders@smartdatainc.net>
2016-01-01 21:39:19 +0530

    New File for Partner search and connect

---
+++ AutomationforSalesforce/src/test/java/pageObjects/SF_SearchAndConnectPartner.java
@@ -0,0 +1,13 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class SF_SearchAndConnectPartner {
+	
+	private WebDriver driver;
+	
+	public SF_SearchAndConnectPartner(WebDriver driver){
+		this.driver=driver;
+	}
+
+}
